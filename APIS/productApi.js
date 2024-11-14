7d8b013daf400e5d2e2e2da33e9c5df6aca31b51
Durga Prasad <ravidurgaprasad160@gmail.com>
2024-11-14 14:50:04 +0530

    CRUD

--- APIS/productApi.js
+++ APIS/productApi.js
@@ -1,19 +1,44 @@
 const exp = require('express')
 const productApp = exp.Router()
 
+// importing expressAsyncHandler
+const expressAsyncHandler = require('express-async-handler')
+
 //to extract the body
 productApp.use(exp.json())
 
 // to get all products
-productApp.get('/getproducts', (req,res)=>{
-    res.send({message:'to get all products'})
-})
+productApp.get('/getproducts', expressAsyncHandler(async(req,res)=>{
+    // to get productCollectionObj
+    let productCollectionObj = req.app.get("productCollectionObj")
+    // getting all products
+    let products = await productCollectionObj.find().toArray()
+    if(products == null){
+        res.send({message:"No products exists"})
+    }
+    else{
+        res.send({message:"List of products", payload:products})
+    }
+
+}))
 
 
 // to get selected products
-productApp.get('/getproducts/:id', (req,res)=>{
-    res.send({message:`product with id ${req.params.id}`})
-})
+productApp.get('/getproduct/:id', expressAsyncHandler(async(req,res)=>{
+    //to get productCOllectionObj
+    let productCOllectionObj = req.app.get('productCollectionObj')
+    // getting productId
+    let productId = (+req.params.id)
+    //getting product with Id
+    let product = await productCOllectionObj.findOne({productId:productId})
+    if(product == null){
+        res.send({message:"No product exist with that Id"})
+    }
+    else{
+        res.send({message:`product with id ${productId}`, payload:product})
+    }
+
+}))
 
 productApp.post('/create-product', async(req, res)=>{
     
@@ -29,4 +54,22 @@ productApp.post('/create-product', async(req, res)=>{
     res.send({message:"New product created successfully"})
 })
 
+// to update product
+productApp.put('/update-product', expressAsyncHandler(async(req,res)=>{
+    //get productCollectonObj
+    let productCollectionObj = req.app.get('productCollectionObj')
+    let updatedProduct = req.body
+    //updating the product
+    await productCollectionObj.updateOne({productId:updatedProduct.productId},{$set:{...updatedProduct}})
+}))
+
+// deleting the product
+productApp.delete('/remove-product/:id', expressAsyncHandler(async(req,res)=>{
+    //get productCollectionObj
+    let productCOllectionObj = req.app.get('productCollectionObj')
+    //getting product ID
+    let productId = (+req.params.id)
+    await productCOllectionObj.deleteOne({productId:productId})
+    res.send({message:'product deleted successfully'})
+}))
 module.exports = productApp
